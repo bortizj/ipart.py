21a83a26668187e7a6fc1e0c392b3156c601a87b
Benhur <benhur.ortizjaramillo@gmail.com>
2025-08-23 15:17:31 +0200

    Makes possible to load the full color palete when given none as number of colors

--- ipart/palettes/color_palettes.py
+++ ipart/palettes/color_palettes.py
@@ -30,11 +30,13 @@ class ColorPalette:
     Initializes the color palette.
     """
 
-    def __init__(self, rng: np.random.Generator, color_palette="kaggle", n_colors: int = 100):
+    def __init__(self, rng: np.random.Generator, color_palette="kaggle", n_colors: int | None = 100):
         self.color_palette = color_palette
 
         # Getting the list of colors from file or random generator
         if color_palette == "random":
+            if n_colors is None:
+                n_colors = 100
             rgb_code = rng.integers(0, 255, (n_colors, 3))
         else:
             if color_palette == "dmc":
@@ -46,6 +48,8 @@ class ColorPalette:
             else:
                 raise ValueError(f"Unknown color palette: {color_palette}")
             # Selecting the colors from the file
+            if n_colors is None:
+                n_colors = rgb_code.shape[0]
             if n_colors > rgb_code.shape[0]:
                 n_colors = rgb_code.shape[0]
 
@@ -54,6 +58,7 @@ class ColorPalette:
             rgb_code = rgb_code[idx]
 
         # Normalizing and converting to opencv format
+        self.n_colors = n_colors
         self.bgr_lut = rgb_code[::, ::-1].astype("float32") / 255.0
 
     def lut(self, img: np.ndarray) -> np.ndarray:
